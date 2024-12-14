ebf187da3b87e04f3c8eee807a5adafb9c4ba9c4
GDKipper <kuzniecow667@gmail.com>
2024-12-14 22:16:58 +0300

    Снова ебля с абилками :<

--- src/main/java/net/sashegdev/gribMine/commands/handleWeaponCommand.java
+++ src/main/java/net/sashegdev/gribMine/commands/handleWeaponCommand.java
@@ -46,7 +46,9 @@ public class handleWeaponCommand {
                     }
 
                     lore.add("Редкость: " + (lines.get("rarity") != null ? lines.get("rarity") : "common"));
-                    lore.add("Способность: " + (lines.get("ability") != null ? lines.get("ability") : "none"));
+
+                    lore.add("Способность: " + WeaponManager.getWeaponAbilities().get(lines.get("ability")));
+
                     lore.add("Модификатор урона: " + GribMine.getMineConfig().getDouble("damage_mod." + lines.get("rarity"), 1.0));
 
                     meta.setLore(lore);
